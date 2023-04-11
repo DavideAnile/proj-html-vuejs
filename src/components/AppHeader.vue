2794b03b3ca5e91ba6136f24a6058acdcb2a16b8
DavideAnile <davide.anile16@gmail.com>
2023-04-11 11:03:47 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+
+</script>
+
+<template>
+<div class="header-section">
+    
+        <div class="up-header my-container">
+            <div>ciao</div>
+            <div>ciao</div>
+        </div>
+        <div class="bottom-header">
+            
+        </div>
+    
+
+</div>
+</template>
+
+<style lang="scss" scoped>
+
+.header-section{
+    height: 700px;
+    background-color: aqua;
+
+    .up-header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 5%;
+        background-color: #353637;
+    }
+
+    .bottom-header{
+        height: 95%;
+        
+    }
+
+}
+
+</style>
